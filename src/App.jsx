d4f505e21fd41580a5ddc7b6cced5f40a6148423
Mariano <marianolangge@gmail.com>
2023-04-05 19:21:02 -0300

    rrdom

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import Navbar from "../src/Components/Navbar/Navbar";
 import "./App.css";
-import ProductDetailContainer from "./Components/ProductDetailContainer/ProductDetailContainer";
-import Header from "./Components/Header/Header"
+import Header from "./Components/Header/Header";
 import ProductsContainer from "./Components/ProductsContainer/ProductsContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Header/>
-      <div className="grid grid-cols-12 h-screen ">
-        {/* <div className="col-span-4 md:col-span-2 bg-gray-200 shadow-md"><Navbar/></div> */}
-        <div className="col-span-12 ">
-          <ProductsContainer />
-        </div>
-      </div>
-      {/* <ProductDetailContainer/> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductsContainer />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
