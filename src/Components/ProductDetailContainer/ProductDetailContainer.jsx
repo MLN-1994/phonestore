d85fb249c7df22be72d36bd329869ffe8c80004e
Mariano <marianolangge@gmail.com>
2023-04-22 14:05:02 -0300

    fixs

--- src/Components/ProductDetailContainer/ProductDetailContainer.jsx
+++ src/Components/ProductDetailContainer/ProductDetailContainer.jsx
@@ -18,24 +18,36 @@ const ProductDetailContainer = ({ productId }) => {
         {product && (
           <>
             <>
-              <div className="bg-white flex">
-                <div className="w-2/3 flex justify-center">
-                  <img src={product.image} alt="" />
-                </div>
-                <div className="bg-red-300 w-1/3">
-                  <div className="p-2">
-                    <div className="">
-                    <p className="font-bold text-3xl my-6">{product.name}</p>
+              <div className="flex items-center justify-center">
+                <div className="bg-white md:flex border m-10 shadow-md rounded-md">
+                  <div className="md:w-2/3 flex justify-center">
+                    <img src={product.image} alt="" />
                   </div>
-                  <div className="">
-                    <p>${product.price}</p>
+                  <div className=" md:w-1/3 my-6 mx-6 rounded-md border shadow-md">
+                    <div className="p-2">
+                      <div className="">
+                        <p className="font-bold text-3xl my-6">
+                          {product.name}
+                        </p>
+                        <hr />
+                      </div>
+                      <div className="my-6">
+                        <p className="font-semibold">${product.price}</p>
+                      </div>
+                      <div className="">
+                        <p className="font-semibold">Stock: {product.stock}</p>
+                      </div>
+                      <div className="my-6">
+                        <p className="font-semibold">{product.description}</p>
+                      </div>
+                      <hr />
+                      <div className="">
+                        <button className="bg-gray-800 hover:bg-gray-700 p-2 rounded-md shadow hover:shadow-lg font-semibold text-lg  text-white flex justify-center w-full ">
+                          Comprar
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="">
-                    <p>{product.description}</p>
-                  </div>
-                  </div>
-                  
-
                 </div>
               </div>
             </>
