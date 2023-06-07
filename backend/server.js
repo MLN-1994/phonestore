9d04af5c2b18510774b9f283b7175e296c89f527
Mariano <marianolangge@gmail.com>
2023-06-07 20:07:59 -0300

    back2

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const mysql2 = require("mysql2");
+const cors = require("cors");
+
+const app = express()
+const port = 3000
+
+const allowedOrigins = "*";
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
+
+//conexion base de datos
+const connection = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "37846699a",
+    database: "phonestore",
+  })
+
+  // Establish the connection
+connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }
+    console.log("Connected to MySQL database!");
+  });
+
+ 
+  app.get("/products", (req, res) => {
+    const query = "SELECT * FROM products";
+  
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error("Error retrieving products:", err);
+        res.sendStatus(500);
+        return;
+      }
+      res.json(results);
+    });
+
+    
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
