02bc390e3e0951c375dfeee4ae5cc0c343ae7210
SanGonzalezCarretero <santiagogonzalezc22@gmail.com>
2023-06-13 10:12:56 -0300

    lindos cambios

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
-import TitleLogo from "../TitleLogo/TitleLogo";
-
 
 export default function Header() {
+  const [logged, setLogged] = useState(false);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("token");
+
+    if (userToken) {
+      setLogged(true);
+    }
+  }, [logged]);
+
   return (
     <>
-    <div className="mx-auto w-2/3">
+      <div className="mx-auto w-2/3">
+        <div className="grid grid-cols-12  my-4  items-center">
+          <div className=" col-span-8  ">
+            {/* <TitleLogo /> */}
+            <Link to={"/"} className="flex items-center ">
+              <p className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-purple-700">
+                {" "}
+                Innovate Store{" "}
+              </p>
+              <span className="text-2xl">📱</span>
+            </Link>
+          </div>
 
-         <div className="grid grid-cols-12  my-4  items-center">
-        <div className=" col-span-8  ">
-          {/* <TitleLogo /> */}
-          <Link to={"/"} className="flex items-center ">
-          <p className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-purple-700"> Innovate Store </p>
-          <span className="text-2xl">📱</span>
-          </Link>
-          
-        </div>
-        
-        <div className="col-start-12 ">
-          <CartWidget />
+          {logged && (
+            <Link
+              to="/admin"
+              className="fixed right-4 bottom-4 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Agregar productos
+            </Link>
+          )}
+
+          <div className="col-start-12 ">
+            <CartWidget />
+          </div>
         </div>
       </div>
-    </div>
-   
     </>
   );
 }
